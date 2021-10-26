a3749b38b6cbab388596dd2facf31079c772a1d0
5late <christianrfernandes5@gmail.com>
2021-10-26 10:55:03 -0400

    fix to not show numbers

--- scripts/showlast.sh
+++ scripts/showlast.sh
@@ -1,11 +1,11 @@
 history_lc="$(wc -l < history)"
 
 if [ $# -eq 0 ] ; then
-  history | tail -5
+  history | cut -d' ' -f 4- | tail -5
 else
   if [ $1 -gt $history_lc ] ; then
     echo "History file does not go back that far."
   elif [ $1 -lt $history_lc ] ; then
-    history | tail -$1
+    history | cut -d' ' -f 4- | tail -$1
   fi
 fi
