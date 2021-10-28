a6f048b395cd6d55b10f2f7b070fb0aefe9a9826
5late <christianrfernandes5@gmail.com>
2021-10-28 00:25:28 -0400

    added setting option for checksh bashrc

---
+++ settings.sh
@@ -0,0 +1,17 @@
+echo "Extra settings script"
+echo
+
+if [ $# -eq 0 ]; then
+  echo "Please choose a script to edit settings."
+fi
+
+if [ $1 == "check" ]; then
+  echo "Would you like the 5 most recent notes to show up on login?"
+  read loginchoice
+  if [ $loginchoice == "y" ]; then
+    echo "cat $HOME/.remember | tail -5" >> .bashrc
+    echo
+    sleep 1
+    echo "Updated command."
+  fi
+fi
