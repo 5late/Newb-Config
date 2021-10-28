a8487e2200b6c331921d00017d732e82ca8359b4
5late <christianrfernandes5@gmail.com>
2021-10-28 00:26:18 -0400

    oops, wrong bashrc

--- settings.sh
+++ settings.sh
@@ -9,7 +9,7 @@ if [ $1 == "check" ]; then
   echo "Would you like the 5 most recent notes to show up on login?"
   read loginchoice
   if [ $loginchoice == "y" ]; then
-    echo "cat $HOME/.remember | tail -5" >> .bashrc
+    echo "cat $HOME/.remember | tail -5" >> $HOME/.bashrc
     echo
     sleep 1
     echo "Updated command."
